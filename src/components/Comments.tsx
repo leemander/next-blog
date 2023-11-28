a7f4beb98dd97cbb302cd8066c896444cb0281bb
Lee Mander <lee.mander@gmail.com>
2023-11-28 09:47:44 +0000

    Set up project

---
+++ src/components/Comments.tsx
@@ -0,0 +1,19 @@
+export default async function Comments({ postSlug }: { postSlug: string }) {
+  return (
+    <section>
+      <h3>Comments</h3>
+      <h4>Leave a comment:</h4>
+      <form action={`/api/comments/${postSlug}`} method="POST">
+        <label htmlFor="username">
+          Username:
+          <input type="text" name="username" id="username" />
+        </label>
+        <label htmlFor="comment">
+          Comment:
+          <textarea name="comment" id="comment" cols={30} rows={10} />
+          <button type="submit">Post</button>
+        </label>
+      </form>
+    </section>
+  );
+}
