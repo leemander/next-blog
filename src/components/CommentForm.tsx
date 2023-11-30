7167e89c7b1612f865558fec6907e60bacaa168d
Lee Mander <lee.mander@gmail.com>
2023-11-30 11:09:17 +0000

    Refactor comment form

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,62 +1,28 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
 import { WEB_SITE } from "config";
-
-/// ... imports here
+import FormStatusButton from "./FormStatusButton";
+import { saveComment } from "@/lib/comments";
+// gives us a way to reload page after data updates.
+import { revalidatePath } from "next/cache";
 
 export function CommentForm({ postSlug }: { postSlug: string }) {
-  // the router hook to trigger a page refresh
-  const router = useRouter();
+  console.log("This is running on the browser");
 
-  // the react useTransition hook to manage client/server data upodates
-  // without refreshing the page. isPending gives us the ability to know
-  // show a spinner or similar
-  const [isPending, startTransition] = useTransition();
+  // When our form action fires, this function runs the createComments function. No more visiting our API!
 
-  // runs when onSubmit event fires, uses fetch to send a POST request to
-  // our API comment route, and then refreshes the page data to show the comment
-  async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
-    console.log("submiting the form");
+  async function handleFormSubmit(formData: FormData) {
+    "use server";
+    console.log("submitting the form");
+    const username = formData.get("username") as string;
+    const comment = formData.get("comment") as string;
 
-    // prevent the form submitting and redircting us to the action location
-    event.preventDefault();
-
-    // get the form input values
-    // @ts-ignore
-    const username = event.target["username"].value;
-    // @ts-ignore
-    const comment = event.target["comment"].value;
-
-    // create a FormData object and append our values to send to the API
-    const formData = new FormData();
-    formData.append("username", username);
-    formData.append("comment", comment);
-
-    // POST the FormData to the API
-    const res = await fetch(`${WEB_SITE}/api/comments/${postSlug}`, {
-      body: formData,
-      method: "POST",
-    });
-
-    // Refresh the current route and fetch new data from the server without
-    // losing client-side browser or React state.
-    startTransition(() => {
-      router.refresh();
-      console.log("reloaded the page data");
-    });
-
-    // @ts-ignore
-    event.target["username"].value = "";
-    // @ts-ignore
-    event.target["comment"].value = "";
+    await saveComment(username, comment, postSlug);
+    revalidatePath(`$/blog/${postSlug}`);
   }
 
-  // the handleFormSubmit fuction is passed to the onSubmit event handler on the form
   return (
     <>
       <h3 className="text-2xl font-bold">Leave a comment:</h3>
-      <form className={`grid gap-8`} onSubmit={handleFormSubmit}>
+      <form className={`grid gap-8`} action={handleFormSubmit}>
         <label className="grid gap-2 font-semibold " htmlFor="username">
           Username:
           <input
@@ -76,14 +42,7 @@ export function CommentForm({ postSlug }: { postSlug: string }) {
             rows={10}
           />
         </label>
-
-        <button
-          className="block w-fit px-6 py-4 font-semibold bg-red-500 text-white rounded"
-          type="submit"
-          disabled={isPending}
-        >
-          {isPending ? "Sending comment..." : "Send comment"}
-        </button>
+        <FormStatusButton />
       </form>
     </>
   );
---
+++ src/components/FormStatusButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+
+export default function FormStatusButton() {
+  const { pending } = useFormStatus();
+  //useFormStatus must be used within a form
+  return (
+    <button
+      className="block w-fit px-6 py-4 font-semibold bg-red-500 text-white rounded"
+      type="submit"
+      disabled={pending}
+    >
+      {pending ? "Sending comment..." : "Send comment"}
+    </button>
+  );
+}
