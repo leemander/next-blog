78b4de211195ee892b8ba28cfe1c84bce12b6b72
Lee Mander <lee.mander@gmail.com>
2023-11-30 16:02:59 +0000

    Add about content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,40 +1,23 @@
 import { bitter } from "../layout";
-
+import Image from "next/image";
 export default function Page() {
   return (
     <main className="fade-in">
       <h2 className={`${bitter.className} text-3xl font-bold`}>About Me</h2>
-      <p className="my-4 ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-        minima, dicta praesentium expedita aut ab sed quam aspernatur quibusdam
-        hic consequatur minus repellendus! Earum atque ratione pariatur,
-        perferendis praesentium officia voluptas iusto blanditiis eum provident,
-        repudiandae maxime distinctio rem laboriosam tempora deserunt porro,
-        voluptatum adipisci qui. Ipsa, eaque corrupti distinctio blanditiis
-        necessitatibus quae dignissimos odit magnam vitae aut eveniet, eum
-        ducimus quaerat sit qui vel excepturi? Sapiente possimus consectetur
-        mollitia ipsa pariatur earum numquam quidem fugiat amet suscipit, quis
-        repudiandae velit accusamus commodi aut voluptatibus ea delectus
-        blanditiis! Rerum alias eum optio enim, esse quia reiciendis. Pariatur
-        obcaecati beatae labore nam deleniti impedit soluta natus, saepe ad!
-        Voluptate voluptates dignissimos id veniam. Autem soluta optio quo vero
-        cumque asperiores totam voluptatem officia repellat voluptas veniam enim
-        magnam sapiente, modi iste dolorem expedita maxime sit corporis nemo
-        numquam ut eum architecto? Illo, dolores nobis ipsum pariatur ducimus
-        dicta necessitatibus modi, exercitationem molestias laborum sequi
-        distinctio. Labore enim voluptatem alias quae eos deserunt, voluptas
-        aspernatur dolorum quod sequi ad, unde fugit facilis maiores
-        reprehenderit ipsum, eum earum illum quasi incidunt tenetur numquam
-        provident! Tenetur quas quisquam dicta mollitia ad esse minus placeat
-        quos, tempore, quam quo, cum earum. Dolorum nemo ut assumenda recusandae
-        voluptatibus molestiae blanditiis enim? Magni aliquid omnis impedit eum
-        nemo enim quam debitis eos, expedita est praesentium itaque sequi id
-        voluptatem nulla iure ex esse! Nemo labore quas, accusamus repudiandae
-        nihil odit corrupti non voluptates vero voluptatum laboriosam, nobis
-        enim necessitatibus omnis placeat fugiat tempore delectus deserunt in!
-        Omnis.
+      <Image
+        className="rect-img mx-auto"
+        src="/me.jpeg"
+        alt="The author, Lee Mander"
+        width={712}
+        height={400}
+      />
+      <p className="my-4 w-1/2 mx-auto">
+        Hello! My name is Lee and I made this blog to practice my new Next.js
+        skills while celebrated my love of liquid bread. I hope you like it!
+        Feel free to check out{" "}
+        <a href="https://github.com/leemander">my Github profile</a> to see more
+        of what I've been up to.
       </p>
-      <a href="https://github.com/leemander">Github</a>
     </main>
   );
 }
